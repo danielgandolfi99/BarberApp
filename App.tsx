807e97c77bbf9f05107675ae1b3b685ecc35275a
Daniel <danielgandolfi.10@gmail.com>
2024-04-14 12:12:48 -0300

    fix: Ajuste de navegação e fontes app

--- App.tsx
+++ App.tsx
@@ -9,6 +9,13 @@ import {
   Montserrat_700Bold,
   Montserrat_500Medium,
 } from "@expo-google-fonts/montserrat";
+import {
+  Ubuntu_300Light,
+  Ubuntu_400Regular,
+  Ubuntu_500Medium,
+  Ubuntu_700Bold,
+} from "@expo-google-fonts/ubuntu";
+import CadastroBarbeiros from "./src/pages/gerencial/cadastro-babeiros";
 
 const Stack = createStackNavigator();
 
@@ -25,6 +32,10 @@ export default function App() {
     Montserrat_400Regular,
     Montserrat_700Bold,
     Montserrat_500Medium,
+    Ubuntu_300Light,
+    Ubuntu_400Regular,
+    Ubuntu_500Medium,
+    Ubuntu_700Bold,
   });
 
   if (!fontsLoaded) {
@@ -43,6 +54,10 @@ export default function App() {
         >
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Cadastro" component={Cadastro} />
+          <Stack.Screen
+            name="Cadastro Barbeiros"
+            component={CadastroBarbeiros}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </LinearGradient>
