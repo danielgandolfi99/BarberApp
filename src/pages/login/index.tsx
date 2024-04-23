d267fa2d9485672b7839f8e8f2447cde244752e9
Daniel Gandolfi <danielgandolfi.10@gmail.com>
2024-04-23 18:00:16 -0300

    feat: criado redux para salvar token

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -6,9 +6,13 @@ import TextInputStyled from "../../components/TextInputStyled";
 import { styles } from "../../components/stylesComponents";
 import ButtonStyled from "../../components/ButtonStyled";
 import TextTitleStyled from "../../components/TextTitleStyled";
+import api from "../../services/api";
+import { useDispatch } from "react-redux";
+import { setToken } from "../../services/redux/authSlice";
 
 const Login = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -17,6 +21,21 @@ const Login = () => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   }
+
+  const test = () => {
+    api
+      .post("/auth", {
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        const { access_token } = response.data;
+        dispatch(setToken(access_token));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const handleSubmit = () => {
     if (!email || !password) {
